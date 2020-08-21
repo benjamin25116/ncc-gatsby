b3212d38bfb4418e27d3177884bd540941723e1a
Benjamin25116 <benjamin25116@gmail.com>
2020-08-22 06:31:31 +0800

    added media queries for bannerImage

--- src/components/bannerImage.js
+++ src/components/bannerImage.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import device from "../components/devices"
 
 const BannerImage = () => {
   const data = useStaticQuery(graphql`
@@ -24,12 +26,51 @@ const BannerImage = () => {
   `)
 
   const doc = data.prismic.allBanner_homes.edges[0].node
-  // console.log(doc)
-  return (
-    <a href={doc.external_link.url}>
-      <img src={doc.banner_image.url} alt={doc.alternate_text} />
-    </a>
-  )
+
+  const Hero = styled.div`
+    background: url(${doc.banner_image.url}) top center no-repeat;
+    height: 380px;
+    background-size: auto 320px;
+
+    @media ${device.mobileS} {
+      height: 380px;
+      background-size: auto 380px;
+    }
+    @media ${device.mobileM} {
+      height: 450px;
+      background-size: auto 450px;
+    }
+    @media ${device.mobileL} {
+      height: 470px;
+      background-size: auto 470px;
+    }
+    @media ${device.tablet} {
+      height: 580px;
+      background-size: auto 580px;
+    }
+    @media ${device.laptop} {
+      height: 820px;
+      background-size: auto 820px;
+    }
+    @media ${device.laptopL} {
+      height: 880px;
+      background-size: auto 880px;
+    }
+  `
+
+  // Checking if banner image has a link associated with it. If yes, store it in linkurl.
+  const linkUrl = doc.external_link ? doc.external_link.url : null
+
+  // Check banner to ensure external link exists.
+  if (linkUrl === null) {
+    return <Hero />
+  } else {
+    return (
+      <a href={linkUrl}>
+        <Hero />
+      </a>
+    )
+  }
 }
 
 export default BannerImage
--- src/components/devices.js
+++ src/components/devices.js
@@ -1,7 +1,7 @@
 const size = {
   mobileS: "320px",
   mobileM: "375px",
-  mobileL: "425px",
+  mobileL: "414px",
   tablet: "768px",
   laptop: "1024px",
   laptopL: "1440px",
@@ -16,7 +16,6 @@ const device = {
   laptop: `(min-width: ${size.laptop})`,
   laptopL: `(min-width: ${size.laptopL})`,
   desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`,
 }
 
 export default device
