286911a59b927594948466a13dd589e9217463fe
Benjamin25116 <benjamin25116@gmail.com>
2020-08-23 22:39:40 +0800

    changed background image display, margins and flex

--- src/components/card.js
+++ src/components/card.js
@@ -4,18 +4,22 @@ import Button from "./button"
 
 const StyledArticle = styled.article`
   display: flex;
+  flex-grow: 1;
   justify-content: center;
   align-items: flex-end;
-  height: 250px;
-  width: 250px;
+  height: 200px;
   border-radius: 10px;
   padding-bottom: 1.5rem;
+  margin: 0 0.5rem 1rem 0.5rem;
 `
 
 const Card = ({ color, imgSrc, to, title }) => {
   return (
     <StyledArticle
-      style={{ background: `url(${imgSrc})`, backgroundSize: "250px" }}
+      style={{
+        background: `url(${imgSrc}) center center`,
+        backgroundSize: "cover",
+      }}
     >
       <Button to={to} color={color}>
         {title}
