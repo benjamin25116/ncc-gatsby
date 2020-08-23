ed3a41805cbec9628bfa7f3fdfeeb02c4f340067
Benjamin25116 <benjamin25116@gmail.com>
2020-08-23 22:41:33 +0800

    Basic display for footer; set backround color and max-width

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,14 +1,34 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFooter = styled.footer`
+  background: var(--green);
+  height: 50px;
+  padding: 1rem; 2rem;
+`
+const Copyright = styled.p`
+  font-weight: bold;
+  color: var(--bodyText);
+  max-width: 900px;
+  margin: auto;
+  text-align: right;
+  a {
+    color: var(--bodyText);
+    text-decoration: underline;
+  }
+`
 
 const Footer = () => {
   return (
-    <footer>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
-      {` `}by{` `}
-      <a href="https://www.instagram.com/pitchun_/">@pitchun_</a>
-    </footer>
+    <StyledFooter>
+      <Copyright>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        {` `}by{` `}
+        <a href="https://www.instagram.com/pitchun_/">Ben</a>
+      </Copyright>
+    </StyledFooter>
   )
 }
 
